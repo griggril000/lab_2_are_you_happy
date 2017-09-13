684d5f3376aa54686baf047cb057d4c94ec0752e
Riley Grigg <griggriley@gmail.com>
2017-09-13 15:04:17 +0000

    added userInput variable

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
+#include <string>
 using namespace std;
 int main()
 {
+    bool ishappy;
+    string userInput;
+    cout << "Are you happy? Yes or No?";
+    cin >> userInput;
+    if(userInput == "yes")
+    {
+        ishappy == true;
+    }
 	return 0;
 }
