35789bc323a6bd383f609fb3b52d4a22860ccc57
Riley Grigg <griggriley@gmail.com>
2017-09-13 15:08:45 +0000

    added else

--- Source.cpp
+++ Source.cpp
@@ -5,11 +5,15 @@ int main()
 {
     bool ishappy;
     string userInput;
-    cout << "Are you happy? Yes or No?";
+    cout << "Are you happy? Yes or No?" << endl;
     cin >> userInput;
     if(userInput == "yes")
     {
         ishappy == true;
+    }
+    else
+    {
+        cout << "Well then become happy!";
     }
 	return 0;
 }
